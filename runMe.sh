95d640349dbddd90f566f535f43a496a71fd3fe1
James Casaletto <jcasaletto@maprtech.com>
2019-09-17 15:24:06 -0700

    renaming docker container to brcachallenge/federated-analysis

--- runMe.sh
+++ runMe.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-FDA_PATH=/Users/jcasaletto/PycharmProjects/BIOBANK/federated-analysis
+FDA_PATH=$(pwd)
 
 APP_PATH=${FDA_PATH}/app
 CONF_PATH=${FDA_PATH}/config
 DATA_PATH=${FDA_PATH}/data
 
-#DOCKER_IMAGE_NAME=my_fda_image
-DOCKER_IMAGE_NAME=mytest
+DOCKER_IMAGE_NAME=brcachallenge/federated-analysis
 
 docker run --rm -e PYTHONPATH=/ -e PYTHONIOENCODING=UTF-8 -w / --user=`id -u`:`id -g` -v ${APP_PATH}:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data ${DOCKER_IMAGE_NAME} /usr/bin/python3 /app/dataAnalyzer.py /config/conf.json
 
