4acca34f0f3a98846f40b3457a8bc15265aac119
James Casaletto <jcasaletto@maprtech.com>
2020-03-12 10:47:15 -0700

    fixed runme script to accommodate new cli args

--- runMe.sh
+++ runMe.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if [ $# -ne 1 -a $0 != "-p" -a $0 != "-c" ]
+if [ $# -ne 2 ] 
 then
-	echo "usage: $0 -p|-c|-b"
+	echo "usage: $0 <ensembl-release> <chr-list>"
+	echo "example: $0 75 \"['chr13', 'chr17']\"" 
 	exit 1
 fi
 
-OP=$1
+ENSEMBL_RELEASE=$1
+CHR_LIST=$2
 FDA_PATH=$(pwd)
 APP_PATH=${FDA_PATH}/app
 CONF_PATH=${FDA_PATH}/config
@@ -24,7 +26,7 @@ chmod 1777 ${DATA_PATH}
 
 docker build -t ${DOCKER_IMAGE_NAME} .
 
-docker run --rm -e PYTHONPATH=/ -e PYTHONIOENCODING=UTF-8 -w /home/myuser --user=1968:games -v ${APP_PATH}:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data:rw ${DOCKER_IMAGE_NAME} /usr/bin/python3 /app/myCooccurrenceFinder.py  $OP 
+docker run --rm -e PYTHONPATH=/ -e PYTHONIOENCODING=UTF-8 -w /home/myuser --user=1968:games -v ${APP_PATH}:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data:rw ${DOCKER_IMAGE_NAME} /usr/bin/python3 /app/myCooccurrenceFinder.py  $ENSEMBL_RELEASE $CHR_LIST
 
 #docker run --rm -e PYTHONPATH=/ -e PYTHONIOENCODING=UTF-8 -w / --user=`id -u`:`id -g` -v ${APP_PATH}:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data ${DOCKER_IMAGE_NAME} /usr/bin/python3 /app/dataAnalyzer.py /config/conf.json 
 #docker run -it --rm --user=1968:1968 -w /home/myuser -v "$(pwd)":/app  -v "${DATA_PATH}":/data:rw --entrypoint /bin/bash  ${DOCKER_IMAGE_NAME}
