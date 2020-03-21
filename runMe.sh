829d8856b661a39790d400958d678ec3ab0df1ec
James Casaletto <jcasaletto@maprtech.com>
2020-03-21 09:16:34 -0700

    updated runme for refactored name

--- runMe.sh
+++ runMe.sh
@@ -29,7 +29,7 @@ chmod 1777 ${DATA_PATH}
 
 docker build -t ${DOCKER_IMAGE_NAME} .
 
-docker run --rm -e PYTHONPATH=/ -e PYTHONIOENCODING=UTF-8 -w /home/myuser --user=1968:games -v ${APP_PATH}:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data:rw ${DOCKER_IMAGE_NAME} /usr/bin/python3 /app/myCooccurrenceFinder.py  $HG_VERSION $ENSEMBL_RELEASE $CHR_LIST $GENE_LIST $PHASED
+docker run --rm -e PYTHONPATH=/ -e PYTHONIOENCODING=UTF-8 -w /home/myuser --user=1968:games -v ${APP_PATH}:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data:rw ${DOCKER_IMAGE_NAME} /usr/bin/python3 /app/cooccurrenceFinder.py  $HG_VERSION $ENSEMBL_RELEASE $CHR_LIST $GENE_LIST $PHASED
 
 #docker run --rm -e PYTHONPATH=/ -e PYTHONIOENCODING=UTF-8 -w / --user=`id -u`:`id -g` -v ${APP_PATH}:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data ${DOCKER_IMAGE_NAME} /usr/bin/python3 /app/dataAnalyzer.py /config/conf.json 
 #docker run -it --rm --user=1968:1968 -w /home/myuser -v "$(pwd)":/app  -v "${DATA_PATH}":/data:rw --entrypoint /bin/bash  ${DOCKER_IMAGE_NAME}
