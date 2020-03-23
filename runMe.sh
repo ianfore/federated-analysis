fc3303bf600a3f1d5e8c411426a8746940002859
James Casaletto <jcasaletto@maprtech.com>
2020-03-23 12:23:33 -0700

    fixed some bugs in runme script

--- runMe.sh
+++ runMe.sh
@@ -11,7 +11,7 @@ then
 	echo "usage: $0 <vcf-file> analyze"
 	echo "OR"
 	echo "$0 <input-vcf-filename> <output-json-filename> <hg-version> <ensembl-version> <chromosomes-of-interest> <phased-boolean>"
-	echo "example: $0 BreastCancer.shuffle.vcf myCooccurrences.json 37 75 [13,17] False"
+	echo "example: $0 BreastCancer.shuffle.vcf myCooccurrences.json 37 75 False"
 	exit 1
 	
 
@@ -19,25 +19,25 @@ elif [ $# -eq 1 -a $1 == 'analyze' ]
 then
 	docker run --rm -e PYTHONPATH=/ -e PYTHONIOENCODING=UTF-8 -w / --user=`id -u`:`id -g` -v ${APP_PATH}:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data ${DOCKER_IMAGE_NAME} /usr/bin/python3 /app/dataAnalyzer.py /config/conf.json 
 	exit 0
-elif [ $# -eq 6 ]
+elif [ $# -eq 5 ]
 then
 
 	VCF_FILE=$1
 	OUTPUT_FILE=$2
 	HG_VERSION=$3
 	ENSEMBL_RELEASE=$4
-	CHR_LIST=$5
-	PHASED=$6
+	PHASED=$5
 
+	CHR_LIST=[13,17]
 
 	if [ ! -d ${DATA_PATH}/pyensembl-cache ]
 	then
         	cd $DATA_PATH
         	cat pyens.?? > pyensembl.zip
         	unzip pyensembl.zip
-        	cd -
 		rm pyens.??
 		rm pyensembl.zip
+        	cd -
 	fi
 
 	if [ $(uname) == "Darwin" ]
