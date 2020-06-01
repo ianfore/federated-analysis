ef27c8ab71bc2f130d8a46928ded742490e67418
James Casaletto <jcasaletto@maprtech.com>
2020-06-01 14:03:09 -0700

    fixed roh bug

--- app/variantAnnotator.py
+++ app/variantAnnotator.py
@@ -12,7 +12,9 @@ def main():
 
     vcf = allel.read_vcf(vcfFileName, fields=['samples', 'calldata/GT', 'variants/ALT', 'variants/CHROM',
             'variants/FILTER_PASS', 'variants/ID', 'variants/POS', 'variants/QUAL','variants/REF', 'variants/INFO'])
+
     genoArray = allel.GenotypeArray(vcf['calldata/GT'])
+
     alleleFrequency = genoArray.count_alleles().to_frequencies()
 
     expectedHeterozygosity = allel.heterozygosity_expected(alleleFrequency, ploidy=2)
@@ -29,12 +31,14 @@ def main():
         diff.append(expectedHeterozygosity[i] - observedHeterozygosity[i])
     print('diff: ' + str(diff))
 
-    n = len(vcf['samples'])
-    posArray = np.asarray([i for i in range(n)])
-    isAccessible = np.asarray([True for i in range(n)])
+    numVariants = len(genoArray)
+    numSamples = len(vcf['samples'])
+    posArray = np.asarray([i for i in range(numVariants)])
+    print('pos = ' + str(posArray))
+    isAccessible = np.asarray([True for i in range(numVariants)])
     runsOfHomozygosity = defaultdict()
-    for i in range(n):
-        genoVector = genoArray[i]
+    for i in range(numSamples):
+        genoVector = genoArray[:,i]
         runsOfHomozygosity[i] = allel.roh_mhmm(genoVector, posArray, is_accessible=isAccessible)
 
 
