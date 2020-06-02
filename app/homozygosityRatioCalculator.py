a78f0d6dcb3403ade55dd79670da7d6e0809ed22
James Casaletto <jcasaletto@maprtech.com>
2020-06-02 08:32:32 -0700

    add graphs to ratios

--- app/homozygosityRatioCalculator.py
+++ app/homozygosityRatioCalculator.py
@@ -1,22 +1,38 @@
 import sys
 import json
+import logging
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
 
 def main():
     if len(sys.argv) != 3:
-        print('13-vpi.json 13-hrpi.json')
+        print('13-vpi.json outputDir')
         sys.exit(1)
 
     vpiFileName = sys.argv[1]
-    hrpiFileName = sys.argv[2]
+    outputDir = sys.argv[2]
 
     with open(vpiFileName, 'r') as f:
         vpiDict = json.load(f)
     f.close()
 
     ratios = getRatios(vpiDict)
-    with open(hrpiFileName, 'w') as f:
+    with open(outputDir + '/hrpi.json', 'w') as f:
         json.dump(ratios, f)
     f.close()
+
+    ratioList = list()
+    for i in ratios:
+        ratioList.append(ratios[i])
+
+    binList = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    binPlot(ratioList, 10, "homozygosity density", "number of individuals", float, 3, binList, outputDir, '13-hrpi.png')
 
 
 def getRatios(vpiDict):
@@ -44,6 +60,49 @@ def getRatios(vpiDict):
         else:
             ratioDict[i] = float(homoSum) / float(homoSum + hetSum)
     return ratioDict
+
+def binPlot(theList, binSize, xlabel, ylabel, dtype, sigDigs, binList, outputDir, imageName):
+    customBinList = False
+    if binList is None:
+        sizeOfRange = 0.5 * (min(theList) + max(theList))
+        try:
+            binList = np.arange(min(theList), max(theList), round((1/binSize) * sizeOfRange, sigDigs) , dtype=dtype)
+        except Exception as e:
+            logger.error('error in binPlot np.arange(): ' + str(e))
+            return
+    else:
+        customBinList = True
+    bins = list()
+
+    for element in theList:
+        for i in range(len(binList) - 1):
+            lhs = binList[i]
+            rhs = binList[i + 1]
+            if element >= lhs and element <= rhs:
+                if customBinList:
+                    bins.append(rhs)
+                else:
+                    bins.append(dtype(round(0.5 * (lhs + rhs), sigDigs)))
+                break
+
+    binMax = binList[len(binList)-1]
+    listMax = max(theList)
+    for element in theList:
+        if element > binMax:
+            bins.append(dtype(round(listMax, sigDigs)))
+
+
+
+    df_bins = pd.DataFrame({xlabel: bins})
+    if (len(df_bins) != 0):
+        df_bins.groupby(xlabel, as_index=False).size().plot(kind='bar')
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        #plt.xlim(start, end)
+        #plt.ylim(ymin, ymax)
+        #plt.show()
+        plt.savefig(outputDir + '/' + imageName)
+
 
 if __name__ == "__main__":
     main()
