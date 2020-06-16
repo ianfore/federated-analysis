4ce55c410c3919731cf1c5a8a989cd369cfa6389
James Casaletto <jcasaletto@maprtech.com>
2020-06-15 20:10:21 -0700

    scikit-allele

--- app/variantAnnotator.py
+++ app/variantAnnotator.py
@@ -4,33 +4,49 @@ import numpy as np
 from collections import defaultdict
 import json
 import pandas as pd
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 
 def main():
+    if len(sys.argv) != 3:
+        print('vcf-file-name output-dir')
+        sys.exit(1)
     vcfFileName = sys.argv[1]
     outputDir = sys.argv[2]
 
+    logger.debug('reading ' + vcfFileName)
     vcf = allel.read_vcf(vcfFileName, fields=['samples', 'calldata/GT', 'variants/ALT', 'variants/CHROM',
             'variants/FILTER_PASS', 'variants/ID', 'variants/POS', 'variants/QUAL','variants/REF', 'variants/INFO'])
 
+    logger.debug('reading data into genotype array')
     genoArray = allel.GenotypeArray(vcf['calldata/GT'])
 
+    logger.debug('counting allele frequencies')
     alleleFrequency = genoArray.count_alleles().to_frequencies()
 
+    logger.debug('calculating expected heterozygosity')
     expectedHeterozygosity = allel.heterozygosity_expected(alleleFrequency, ploidy=2)
     #print('expected: ' + str(expectedHeterozygosity))
 
+    logger.debug('calculating observed heterozygosity')
     observedHeterozygosity = allel.heterozygosity_observed(genoArray)
     #print('observed: ' + str(observedHeterozygosity))
 
+    logger.debug('calculating inbreeding coefficient')
     inbreedingCoefficient = allel.inbreeding_coefficient(genoArray)
     #print('ibc: ' + str(inbreedingCoefficient))
 
+    logger.debug('calculating delta heterozygosity')
     diff = list()
     for i in range(len(expectedHeterozygosity)):
         diff.append(expectedHeterozygosity[i] - observedHeterozygosity[i])
     #print('diff: ' + str(diff))
 
+    logger.debug('finding runs of homozygosity')
     numVariants = len(genoArray)
     numSamples = len(vcf['samples'])
     posArray = np.asarray([i for i in range(numVariants)])
@@ -43,14 +59,16 @@ def main():
             runsOfHomozygosity[i] = {'individual': i, 'roh': allel.roh_mhmm(genoVector, posArray, is_accessible=isAccessible)}
 
 
-    with open(outputDir + '/ibc.txt', 'w') as f:
-        f.write(inbreedingCoefficient)
-    f.close()
+    logger.debug('saving ibc.txt')
+    np.savetxt(outputDir + '/ibc.txt', inbreedingCoefficient)
 
+    logger.debug('saving zygosityDelta.txt')
     with open(outputDir + '/zygosityDelta.txt', 'w') as f:
-        f.write(diff)
+        for item in diff:
+            f.write("%s\n" % item)
     f.close()
 
+    logger.debug('roh.json')
     with open(outputDir + '/roh.json', 'w') as f:
         pd.DataFrame(runsOfHomozygosity).to_csv(f)
     f.close()
