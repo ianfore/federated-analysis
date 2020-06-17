356fe96601491457edee5b599d03c662f954d7c8
James Casaletto <jcasaletto@maprtech.com>
2020-06-17 11:43:31 -0700

    splitting out into 2 docker files - one for cooccur and one for pathology

--- runMe.sh
+++ runMe.sh
@@ -4,7 +4,8 @@ FDA_PATH=$(pwd)
 APP_PATH=${FDA_PATH}/app
 CONF_PATH=${FDA_PATH}/config
 DATA_PATH=${FDA_PATH}/data
-DOCKER_IMAGE_NAME=brcachallenge/federated-analysis
+COOCCUR_DOCKER_IMAGE_NAME=brcachallenge/federated-analysis-coccurrence
+PATHOLOGY_DOCKER_IMAGE_NAME=brcachallenge/federated-analysis-pathology
 
 if [ $# -eq 0 ]
 then
@@ -17,7 +18,10 @@ then
 
 elif [ $# -eq 1 -a $1 == 'analyze' ]
 then
-	docker run --rm -e PYTHONPATH=/ -e PYTHONIOENCODING=UTF-8 -w / --user=`id -u`:`id -g` -v ${APP_PATH}/pathology:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data ${DOCKER_IMAGE_NAME} /usr/bin/python3 /app/dataAnalyzer.py /config/conf.json 
+
+	docker build -t ${PATHOLOGY_DOCKER_IMAGE_NAME} - < Dockerfile-pathology
+
+	docker run --rm -e PYTHONPATH=/ -e PYTHONIOENCODING=UTF-8 -w / --user=`id -u`:`id -g` -v ${APP_PATH}/pathology:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data ${PATHOLOGY_DOCKER_IMAGE_NAME} /usr/bin/python3 /app/dataAnalyzer.py /config/conf.json 
 	exit 0
 
 elif [ $# -eq 11 ]
@@ -36,9 +40,9 @@ then
 	VPI_FILE=${11}
 
 
-	docker build -t ${DOCKER_IMAGE_NAME} - < Dockerfile 
+	docker build -t ${COOCCUR_DOCKER_IMAGE_NAME} - < Dockerfile-cooccurrence
 
-	docker run --rm -e PYTHONPATH=/ -e PYTHONIOENCODING=UTF-8 --user=`id -u`:`id -g` -v ${APP_PATH}/cooccurrence:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data:rw ${DOCKER_IMAGE_NAME} /usr/bin/python3 /app/cooccurrenceFinder.py  --v ${VCF_FILE} --o ${OUTPUT_FILE} --h $HG_VERSION --e $ENSEMBL_RELEASE --c $CHROM --p $PHASED --s ${SAVE_VARS} --g $GENE --b $BRCA_VARS  --d /var/tmp/pyensembl-cache --ipv $IPV_FILE  --vpi $VPI_FILE
+	docker run --rm -e PYTHONPATH=/ -e PYTHONIOENCODING=UTF-8 --user=`id -u`:`id -g` -v ${APP_PATH}/cooccurrence:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data:rw ${COOCCUR_DOCKER_IMAGE_NAME} /usr/bin/python3 /app/cooccurrenceFinder.py  --v ${VCF_FILE} --o ${OUTPUT_FILE} --h $HG_VERSION --e $ENSEMBL_RELEASE --c $CHROM --p $PHASED --s ${SAVE_VARS} --g $GENE --b $BRCA_VARS  --d /var/tmp/pyensembl-cache --ipv $IPV_FILE  --vpi $VPI_FILE
 
 
 
@@ -46,10 +50,10 @@ elif [ $# -eq 2 -a $1 == 'interactive' ]
 then
 	if [ $2 == 'root' ]
 	then
-		docker run -it --rm --user=0:0 -w /home/myuser -v "$(pwd)":/app  -v "${DATA_PATH}":/data:rw --entrypoint /bin/bash  ${DOCKER_IMAGE_NAME}
+		docker run -it --rm --user=0:0 -w /home/myuser -v "$(pwd)":/app  -v "${DATA_PATH}":/data:rw --entrypoint /bin/bash  ${COOCCUR_DOCKER_IMAGE_NAME}
 
 	else
-		docker run -it --rm --user=1968:1968 -w /home/myuser -v "$(pwd)":/app  -v "${DATA_PATH}":/data:rw --entrypoint /bin/bash  ${DOCKER_IMAGE_NAME}
+		docker run -it --rm --user=1968:1968 -w /home/myuser -v "$(pwd)":/app  -v "${DATA_PATH}":/data:rw --entrypoint /bin/bash  ${COOCCUR_DOCKER_IMAGE_NAME}
 	fi
 
 else
