45900ac8641490a5e2a01e1c84ed92370756e977
James Casaletto <jcasaletto@maprtech.com>
2020-07-19 09:44:14 -0700

    report

--- app/cooccurrence/finalReport.py
+++ app/cooccurrence/finalReport.py
@@ -8,8 +8,8 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 def main():
-	if len(sys.argv) != 4:
-		print('ipv.json in.txt not.txt')
+	if len(sys.argv) != 5:
+		print('ipv.json in.txt not.txt sites.tsv')
 		sys.exit(1)
 
 
@@ -32,6 +32,13 @@ def main():
 	outList = f.readlines() 
 	f.close()
 	outList = [x.strip() for x in outList]
+
+	sitesFileName = sys.argv[4]
+	logger.info('reading data from ' + sitesFileName)
+	f = open(sitesFileName, 'r')
+	sitesDF = pd.read_csv(sitesFileName, header=0, sep='\t')
+	f.close()
+
 
 	allVariants = ipvDict.keys()
 	variantsDict = dict()
@@ -76,80 +83,54 @@ def main():
 	print(variantsDF.columns)
 	variantsDF.to_csv('/tmp/brcaDF.tsv', sep='\t', index=True)
 
+	variantsWithInfoDF = addInfo(variantsDF, sitesDF)
+
+	variantsWithInfoDF.to_csv('/tmp/brcaDF.tsv', sep='\t', index=False)
 
 
+def addInfo(variantsDF, sitesDF):
 
-def addInfo(variantsDF):
-	brca1DF = pd.read_csv('/Users/jcasaletto/Desktop/TOPMED/f5_copd_brca1_hmb_phased_report.tsv', header=0, sep='\t')
-	brca2DF = pd.read_csv('/Users/jcasaletto/Desktop/TOPMED/f5_copd_brca2_hmb_phased_report.tsv', header=0, sep='\t')
 
-	brca1DF_sites = pd.read_csv('/Users/jcasaletto/Desktop/TOPMED/f5_chr17_brca1_sites.tsv', header=0, sep='\t')
-	brca2DF_sites = pd.read_csv('/Users/jcasaletto/Desktop/TOPMED/f5_chr13_brca2_sites.tsv', header=0, sep='\t')
-
-	brca1Variants = list(brca1DF['variant'])
-	brca1_dict = dict()
-	for index, row in brca1DF_sites.iterrows():
+	variants = list(variantsDF['variant'])
+	brca_dict = dict()
+	for index, row in sitesDF.iterrows():
 		var = str((str(row['#CHROM']).split('chr')[1], row['POS'], row['REF'], row['ALT']))
 		var = var.replace("'", "").replace(" ", "")
-		if var in brca1Variants:
-			brca1_dict[var] = row['INFO']
+		if var in variants:
+			brca_dict[var] = row['INFO']
 
-	brca2Variants = list(brca2DF['variant'])
-	brca2_dict = dict()
-	for index, row in brca2DF_sites.iterrows():
-		var = str((str(row['#CHROM']).split('chr')[1], row['POS'], row['REF'], row['ALT']))
-		var = var.replace("'", "").replace(" ", "")
-		if var in brca2Variants:
-			brca2_dict[var] = row['INFO']
+	info_df = pd.DataFrame(brca_dict.items())
+	info_df.columns = ['variant', 'INFO']
 
-	brca1_info_df = pd.DataFrame(brca1_dict.items())
-	brca1_info_df.columns = ['variant', 'INFO']
-	brca2_info_df = pd.DataFrame(brca2_dict.items())
-	brca2_info_df.columns = ['variant', 'INFO']
 
-	brca1DF_withInfo = pd.merge(brca1DF, brca1_info_df, on='variant', how='left')
-	brca2DF_withInfo = pd.merge(brca2DF, brca2_info_df, on='variant', how='left')
+	finalDF = pd.merge(variantsDF, info_df, on='variant', how='left')
 
-	# when we run SQL commands on a DF, SQL will barf if one col named 'aa' and another 'Aa'
-	# b/c column names in SQL are case-INsensitive
-	# rename the cols ['aa', 'Aa', 'AA'] to ['homo_alt', 'het', 'homo_ref']
-	intermediateCols = ['variant', 'class', 'popFreq', 'cohortFreq', 'homo_alt', 'het', 'homo_ref', 'homozygousSample',
-						'inGnomad', 'INFO']
 
 	# now iterate through the INFO column and pull out each var=val pair
 	# we'll make new cols based on these pairs
-	varValPairs = brca1DF_withInfo.iloc[0]['INFO'].split(';')
-	infoCols = list()
-	for vv in varValPairs:
-		infoCols.append(vv.split('=')[0])
 
-	finalCols = intermediateCols + infoCols
-
-	infoDict_1 = dict()
-	for i in range(len(brca1DF_withInfo.index)):
-		infoDict_1[i] = dict()
-		infoPairs = brca1DF_withInfo.iloc[i]['INFO'].split('|')[0].split(';')
+	infoDict = dict()
+	for i in range(len(finalDF.index)):
+		infoDict[i] = dict()
+		infoPairs = finalDF.iloc[i]['INFO'].split('|')[0].split(';')
 		for pair in infoPairs:
 			vv = pair.split('=')
-			infoDict_1[i][vv[0]] = vv[1]
+			infoDict[i][vv[0]] = vv[1]
 
-	# brca1DF_withInfo[vv[0]] = pd.Series(infoDict[i], index=brca1DF_withInfo.index)
 
-	infoDF_1 = pd.DataFrame.from_dict(infoDict_1).transpose()
-	print(infoDF_1.columns)
-	print(infoDF_1['ABE'])
+	infoDF = pd.DataFrame.from_dict(infoDict).transpose()
 
-	brca1DF_withInfo['FIBC_I'] = infoDF_1['FIBC_I']
-	brca1DF_withInfo['HWEAF_p'] = infoDF_1['HWEAF_P']
-	brca1DF_withInfo['HWE_SLP_I'] = infoDF_1['HWE_SLP_I']
-	brca1DF_withInfo['HWE_SLP_P'] = infoDF_1['HWE_SLP_P']
-	brca1DF_withInfo['AC'] = infoDF_1['AC']
-	brca1DF_withInfo['AF'] = infoDF_1['AF']
-	brca1DF_withInfo['AN'] = infoDF_1['AN']
+	finalDF['FIBC_I'] = infoDF['FIBC_I']
+	finalDF['HWEAF_p'] = infoDF['HWEAF_P']
+	finalDF['HWE_SLP_I'] = infoDF['HWE_SLP_I']
+	finalDF['HWE_SLP_P'] = infoDF['HWE_SLP_P']
+	finalDF['AC'] = infoDF['AC']
+	finalDF['AF'] = infoDF['AF']
+	finalDF['AN'] = infoDF['AN']
 
-	brca1DF_noInfo = brca1DF_withInfo.drop(columns=['INFO'])
+	finalDF = finalDF.drop(columns=['INFO'])
 
-	brca1DF_noInfo.to_csv('/tmp/brca1DF.tsv', sep='\t', index=False)
+	return finalDF
 		
 	
 if __name__ == "__main__":
