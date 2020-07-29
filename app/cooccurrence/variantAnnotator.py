e7147778a26f32f4c2a38ae1fcdc2965a4d3c8c2
James Casaletto <jcasaletto@maprtech.com>
2020-07-28 19:13:12 -0700

    roh

--- app/cooccurrence/variantAnnotator.py
+++ app/cooccurrence/variantAnnotator.py
@@ -13,10 +13,10 @@ logger.setLevel(logging.DEBUG)
 
 def main():
     if len(sys.argv) != 3:
-        print('vcf-file-name output-dir')
+        print('vcf-file-name output-file')
         sys.exit(1)
     vcfFileName = sys.argv[1]
-    outputDir = sys.argv[2]
+    outputFile = sys.argv[2]
 
     logger.debug('reading ' + vcfFileName)
     vcf = allel.read_vcf(vcfFileName, fields=['samples', 'calldata/GT', 'variants/ALT', 'variants/CHROM',
@@ -70,9 +70,9 @@ def main():
             f.write("%s\n" % item)
     f.close()'''
 
-    logger.debug('writing roh.csv')
+    logger.debug('writing to' + outputFile)
     #roh = json.dumps(runsOfHomozygosity)
-    with open(outputDir + '/roh.csv', 'w') as f:
+    with open(outputFile, 'w') as f:
         json.dump(runsOfHomozygosity, f)
     f.close()
 
