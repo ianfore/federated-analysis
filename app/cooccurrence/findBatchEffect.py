167e426ee195a5f5097027360e461af1caf9365a
James Casaletto <jcasaletto@maprtech.com>
2020-08-13 11:05:21 -0700

    merge 13 and 17

--- app/cooccurrence/findBatchEffect.py
+++ app/cooccurrence/findBatchEffect.py
@@ -26,22 +26,42 @@ class NpEncoder(json.JSONEncoder):
 
 def main():
     # read in vpi
-    if len(sys.argv) != 3:
-        print('usage: findBatchEffect vpi.json output-dir')
+    if len(sys.argv) != 4:
+        print('usage: findBatchEffect 13-vpi.json 17-vpi.json output-dir')
         sys.exit(1)
-    vpiFileName = sys.argv[1]
-    centersPerHomoVUSOutputFileName = sys.argv[2] + '/centersPerHomoVUS.json'
-    countsPerCenterOutputFileName = sys.argv[2] + '/countsPerCenter.json'
+    vpi13FileName = sys.argv[1]
+    vpi17FileName = sys.argv[2]
+    centersPerHomoVUSOutputFileName = sys.argv[3] + '/centersPerHomoVUS.json'
+    countsPerCenterOutputFileName = sys.argv[3] + '/countsPerCenter.json'
 
-    logger.info('reading data from ' + vpiFileName)
-    with open(vpiFileName, 'r') as f:
-        vpiDict = json.load(f)
+    logger.info('reading data from ' + vpi13FileName)
+    with open(vpi13FileName, 'r') as f:
+        vpi13Dict = json.load(f)
+    f.close()
+
+    logger.info('reading data from ' + vpi17FileName)
+    with open(vpi17FileName, 'r') as f:
+        vpi17Dict = json.load(f)
     f.close()
 
     centersPerHomoVus = defaultdict(set)
     countsPerCenter = dict()
-    for individual in vpiDict:
-        for vus in vpiDict[individual]['vus']:
+    
+    for individual in vpi13Dict:
+        for vus in vpi13Dict[individual]['vus']:
+            variant = vus[0]
+            genotype = vus[1]
+            seqCenter = vus[2]
+            if not seqCenter in countsPerCenter:
+                countsPerCenter[seqCenter] = {'homoVUS': 0, 'heteroVUS': 0}
+            if genotype == '3':
+                centersPerHomoVus[str(tuple(variant))].add(seqCenter)
+                countsPerCenter[seqCenter]['homoVUS'] += 1
+            else:
+                countsPerCenter[seqCenter]['heteroVUS'] += 1
+
+    for individual in vpi17Dict:
+        for vus in vpi17Dict[individual]['vus']:
             variant = vus[0]
             genotype = vus[1]
             seqCenter = vus[2]
