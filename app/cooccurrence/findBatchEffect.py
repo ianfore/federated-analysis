c9e30363b6ee9516e1fd7001c060aefe5950e5b6
James Casaletto <jcasaletto@maprtech.com>
2020-08-13 17:18:42 -0700

    add study

--- app/cooccurrence/findBatchEffect.py
+++ app/cooccurrence/findBatchEffect.py
@@ -32,7 +32,9 @@ def main():
     vpi13FileName = sys.argv[1]
     vpi17FileName = sys.argv[2]
     centersPerHomoOutputFileName = sys.argv[3] + '/centersPerHomo.json'
+    studiesPerHomoOutputFileName = sys.argv[3] + '/studiesPerHomo.json'
     countsPerCenterOutputFileName = sys.argv[3] + '/countsPerCenter.json'
+    countsPerStudyOutputFileName = sys.argv[3] + '/countsPerStudy.json'
 
     logger.info('reading data from ' + vpi13FileName)
     with open(vpi13FileName, 'r') as f:
@@ -45,38 +47,58 @@ def main():
     f.close()
 
     centersPerHomo = defaultdict(set)
+    studiesPerHomo = defaultdict(set)
     countsPerCenter = dict()
+    countsPerStudy = dict()
 
     for individual in vpi13Dict:
         for vus in vpi13Dict[individual]['vus']:
             variant = vus[0]
             genotype = vus[1]
             seqCenter = vus[2]
+            study = vus[3]
             if not seqCenter in countsPerCenter:
                 countsPerCenter[seqCenter] = {'homoVUS': 0, 'heteroVUS': 0, 'homoBen': 0, 'heteroBen': 0,
                                               'totalHomo': 0, 'totalHetero': 0}
+            if not study in countsPerStudy:
+                countsPerStudy[study] = {'homoVUS': 0, 'heteroVUS': 0, 'homoBen': 0, 'heteroBen': 0,
+                                              'totalHomo': 0, 'totalHetero': 0}
             if genotype == '3':
                 centersPerHomo[str(tuple(variant))].add(seqCenter)
+                studiesPerHomo[str(tuple(variant))].add(study)
                 countsPerCenter[seqCenter]['homoVUS'] += 1
                 countsPerCenter[seqCenter]['totalHomo'] += 1
+                countsPerStudy[study]['homoVUS'] += 1
+                countsPerCenter[study]['totalHomo'] += 1
             else:
                 countsPerCenter[seqCenter]['heteroVUS'] += 1
                 countsPerCenter[seqCenter]['totalHetero'] += 1
+                countsPerStudy[study]['heteroVUS'] += 1
+                countsPerStudy[study]['totalHetero'] += 1
 
         for ben in vpi13Dict[individual]['benign']:
             variant = ben[0]
             genotype = ben[1]
             seqCenter = ben[2]
+            study = ben[3]
             if not seqCenter in countsPerCenter:
                 countsPerCenter[seqCenter] = {'homoVUS': 0, 'heteroVUS': 0, 'homoBen': 0, 'heteroBen': 0,
                                               'totalHomo': 0, 'totalHetero': 0}
+            if not study in countsPerStudy:
+                countsPerStudy[study] = {'homoVUS': 0, 'heteroVUS': 0, 'homoBen': 0, 'heteroBen': 0,
+                                              'totalHomo': 0, 'totalHetero': 0}
             if genotype == '3':
                 centersPerHomo[str(tuple(variant))].add(seqCenter)
+                studiesPerHomo[str(tuple(variant))].add(study)
                 countsPerCenter[seqCenter]['homoBen'] += 1
                 countsPerCenter[seqCenter]['totalHomo'] += 1
+                countsPerStudy[study]['homoVUS'] += 1
+                countsPerCenter[study]['totalHomo'] += 1
             else:
                 countsPerCenter[seqCenter]['heteroBen'] += 1
                 countsPerCenter[seqCenter]['totalHetero'] += 1
+                countsPerStudy[study]['heteroVUS'] += 1
+                countsPerStudy[study]['totalHetero'] += 1
 
 
     for individual in vpi17Dict:
@@ -84,31 +106,49 @@ def main():
             variant = vus[0]
             genotype = vus[1]
             seqCenter = vus[2]
+            study = vus[3]
             if not seqCenter in countsPerCenter:
                 countsPerCenter[seqCenter] = {'homoVUS': 0, 'heteroVUS': 0, 'homoBen': 0, 'heteroBen': 0,
                                               'totalHomo': 0, 'totalHetero': 0}
+            if not study in countsPerStudy:
+                countsPerStudy[study] = {'homoVUS': 0, 'heteroVUS': 0, 'homoBen': 0, 'heteroBen': 0,
+                                              'totalHomo': 0, 'totalHetero': 0}
             if genotype == '3':
                 centersPerHomo[str(tuple(variant))].add(seqCenter)
+                studiesPerHomo[str(tuple(variant))].add(study)
                 countsPerCenter[seqCenter]['homoVUS'] += 1
                 countsPerCenter[seqCenter]['totalHomo'] += 1
+                countsPerStudy[study]['homoVUS'] += 1
+                countsPerCenter[study]['totalHomo'] += 1
             else:
                 countsPerCenter[seqCenter]['heteroVUS'] += 1
                 countsPerCenter[seqCenter]['totalHetero'] += 1
+                countsPerStudy[study]['heteroVUS'] += 1
+                countsPerStudy[study]['totalHetero'] += 1
 
         for ben in vpi17Dict[individual]['benign']:
             variant = ben[0]
             genotype = ben[1]
             seqCenter = ben[2]
+            study = ben[3]
             if not seqCenter in countsPerCenter:
                 countsPerCenter[seqCenter] = {'homoVUS': 0, 'heteroVUS': 0, 'homoBen': 0, 'heteroBen': 0,
                                               'totalHomo': 0, 'totalHetero': 0}
+            if not study in countsPerStudy:
+                countsPerStudy[study] = {'homoVUS': 0, 'heteroVUS': 0, 'homoBen': 0, 'heteroBen': 0,
+                                              'totalHomo': 0, 'totalHetero': 0}
             if genotype == '3':
                 centersPerHomo[str(tuple(variant))].add(seqCenter)
+                studiesPerHomo[str(tuple(variant))].add(study)
                 countsPerCenter[seqCenter]['homoBen'] += 1
                 countsPerCenter[seqCenter]['totalHomo'] += 1
+                countsPerStudy[study]['homoVUS'] += 1
+                countsPerCenter[study]['totalHomo'] += 1
             else:
                 countsPerCenter[seqCenter]['heteroBen'] += 1
                 countsPerCenter[seqCenter]['totalHetero'] += 1
+                countsPerStudy[study]['heteroVUS'] += 1
+                countsPerStudy[study]['totalHetero'] += 1
 
     with open(centersPerHomoOutputFileName, 'w') as f:
         json.dump(centersPerHomo, f, cls=NpEncoder)
@@ -116,6 +156,14 @@ def main():
 
     with open(countsPerCenterOutputFileName, 'w') as f:
         json.dump(countsPerCenter, f, cls=NpEncoder)
+    f.close()
+
+    with open(studiesPerHomoOutputFileName, 'w') as f:
+        json.dump(studiesPerHomo, f, cls=NpEncoder)
+    f.close()
+
+    with open(countsPerStudyOutputFileName, 'w') as f:
+        json.dump(countsPerStudy, f, cls=NpEncoder)
     f.close()
 
 if __name__ == "__main__":
