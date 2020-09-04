de5bbb7a6e4848464759c5680c9e4895f4d6f2d5
James Casaletto <jcasaletto@maprtech.com>
2020-09-03 23:16:33 -0700

    egg

--- app/cooccurrence/eggReport.py
+++ app/cooccurrence/eggReport.py
@@ -4,14 +4,16 @@ import json
 
 
 def main():
-    if len(sys.argv) != 3:
-        print('input-dir output-dir')
+    if len(sys.argv) != 4:
+        print('input-dir brca-variants.tsv output-dir')
         sys.exit(1)
 
     inputDir =sys.argv[1]
     outputDir = sys.argv[2]
+    brcaVariantsFile = sys.argv[3]
     df1 = pd.read_csv(inputDir + '/f8_chr17_brca1_gruhmb_report.tsv', sep='\t')
     df2 = pd.read_csv(inputDir + '/f8_chr13_brca2_gruhmb_report.tsv', sep='\t')
+    brcaDF = pd.read_csv(brcaVariantsFile, sep='\t', header=0)
 
     # read .txt files
     onlyHomo_13_file = open(inputDir + "/13-only-homo.txt", "r")
@@ -32,9 +34,9 @@ def main():
     cols = ['variant', 'popFreq', 'cohortFreq', 'homo_alt', 'hetero', 'homo_ref', 'p', 'q', 'exonic', 'inGnomad']
     eggReport_13 = pd.DataFrame(columns = cols)
     varTypeList = list()
-
-
+    otherClassList = list()
     for i in range(len(df2)):
+        otherClass = "NA"
         x = df2.iloc[i]['variant']
         if x in onlyHomo_13:
             eggReport_13 = eggReport_13.append(df2.iloc[i][cols])
@@ -45,14 +47,22 @@ def main():
         if x in both_13:
             eggReport_13 = eggReport_13.append(df2.iloc[i][cols])
             varTypeList.append('both')
+        pos = x[1]
+        try:
+            otherClass = brcaDF.loc[brcaDF['Pos'] == pos]['Pathogenicity_all']
+        except Exception as e:
+            pass
+        otherClassList.append(otherClass)
 
     eggReport_13['varType'] = varTypeList
     eggReport_13.to_csv(outputDir + '/f8-brca2-forEGG.tsv', index=False, header=True, sep='\t')
+    eggReport_13['otherClass'] = otherClassList
 
     eggReport_17 = pd.DataFrame(columns = cols)
     varTypeList = list()
-
+    otherClassList = list()
     for i in range(len(df1)):
+        otherClass = "NA"
         x = df1.iloc[i]['variant']
         if x in onlyHomo_17:
             eggReport_17 = eggReport_17.append(df1.iloc[i][cols])
@@ -63,9 +73,16 @@ def main():
         if x in both_17:
             eggReport_17 = eggReport_17.append(df1.iloc[i][cols])
             varTypeList.append('both')
+        pos = x[1]
+        try:
+            otherClass = brcaDF.loc[brcaDF['Pos'] == pos]['Pathogenicity_all']
+        except Exception as e:
+            pass
+        otherClassList.append(otherClass)
 
     eggReport_17['varType'] = varTypeList
     eggReport_17.to_csv(outputDir + '/f8-brca1-forEGG.tsv', index=False, sep='\t', header=True)
+    eggReport_17['otherClass'] = otherClassList
 
 if __name__ == "__main__":
     main()
