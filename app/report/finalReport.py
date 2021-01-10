7aef98048f6e30a565e034f459fd6962128ecd50
James Casaletto <jcasaletto@maprtech.com>
2021-01-09 22:57:17 -0800

    module not found error

--- app/report/finalReport.py
+++ app/report/finalReport.py
@@ -6,7 +6,7 @@ import argparse
 import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
-from app.utils.coord2hgvs import coordinateMapper
+from utils.coord2hgvs import coordinateMapper
 
 '''import hgvs.assemblymapper
 import hgvs.dataproviders.uta
