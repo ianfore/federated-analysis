a1251558970728e945a3847e1a594ad7202bb9ae
James Casaletto <jcasaletto@maprtech.com>
2021-01-09 22:51:37 -0800

    module not found error

--- app/report/finalReport.py
+++ app/report/finalReport.py
@@ -4,7 +4,7 @@ import logging
 import hail as hl
 import argparse
 import sys, os
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 from app.utils.coord2hgvs import coordinateMapper
 
