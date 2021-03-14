df37ed88d3fab7c0398eeb4fcfde84661c045333
James Casaletto <jcasaletto@maprtech.com>
2021-03-14 16:36:33 -0700

    throwawy code to get samples for variants

--- app/utils/getSamplesForVariants.py
+++ app/utils/getSamplesForVariants.py
@@ -6,10 +6,12 @@ def main():
     parser = argparse.ArgumentParser(usage="getSamplesForReads")
     parser.add_argument("--v", dest="variantFileName", help="variant file name", default=None)
     parser.add_argument("--i", dest="ipvFileName", help="ipv file name", default=None)
+    parser.add_argument("--g", dest="gen3FileName", help="gen3 file name", default=None)
     parser.add_argument("--o", dest="outputFileName", help="output file name", default=None)
     options = parser.parse_args()
     variantFileName = options.variantFileName
     ipvFileName = options.ipvFileName
+    gen3FileName = options.gen3FileName
     outputFileName = options.outputFileName
 
     variantFile = open(variantFileName)
@@ -20,6 +22,12 @@ def main():
         ipv = json.load(f)
     f.close()
 
+    gen3File = open(gen3FileName)
+    gen3 = set(gen3File.readlines())
+    gen3File.close()
+
+    variant2samplesDict = dict()
+
     for variant in variants:
         variant = variant.replace('(', '').replace(')', '')
         variantArray = variant.split(',')
@@ -28,7 +36,19 @@ def main():
         ref = str(variantArray[2])
         alt = str(variantArray[3].replace('\n', ''))
         variantString = str((chrom, pos, ref, alt))
-        print(variantString)
+
+        variant2samplesDict[variant] = dict()
+        # now get the samples for the variant from ipv
+        homoSamples = set(ipv[variantString]['homozygous individuals'])
+        if len(homoSamples) == 0:
+            variant2samplesDict[variant]['homo'] = None
+        elif len(gen3.intersection(homoSamples)) == 0:
+            variant2samplesDict[variant]['homo'] = None
+        else:
+            variant2samplesDict[variant]['homo'] = gen3.intersection(homoSamples)[0]
+
+    print(variant2samplesDict)
+
 
 
 
