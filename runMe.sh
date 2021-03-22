fc08ef6040ca283a27ab9464de66bec5c5877355
James Casaletto <jcasaletto@maprtech.com>
2021-03-22 15:13:19 -0700

    make runme easier

--- runMe.sh
+++ runMe.sh
@@ -1,60 +1,28 @@
 #!/bin/bash
 
-FDA_PATH=$(pwd)
-APP_PATH=${FDA_PATH}/app
-CONF_PATH=${FDA_PATH}/config
-DATA_PATH=${FDA_PATH}/data
-COOCCUR_DOCKER_IMAGE_NAME=brcachallenge/federated-analysis:cooccurrence
-PATHOLOGY_DOCKER_IMAGE_NAME=brcachallenge/federated-analysis:pathology
 
-if [ $# -eq 0 ]
-then
-	echo "usage: $0 <vcf-file> analyze"
-	echo "OR"
-	echo "$0 <input-vcf-filename> <output-json-filename> <hg-version> <ensembl-version> <chromosome-of-interest> <phased-boolean> <gene-of-interest> <save-variants> <brca-vars-filename>" 
-	echo "example: $0 /data/bc3.vcf /data/myout.json 38 99 13 True BRCA2 True /data/brca-variants.tsv" 
-	exit 1
-	
+DATA_DIR=/private/groups/patenlab/jcasalet/TOPMED_WORKDIR/DATA/PUB/MANDY/BROAD/13/AMR
+CHROM=13
+GENE=BRCA2
+VCF_FILENAME=f8_chr13_brca2_amr-broad.vcf
 
-elif [ $# -eq 1 -a $1 == 'analyze' ]
-then
-
-	docker build -t ${PATHOLOGY_DOCKER_IMAGE_NAME} - < docker/pathology/Dockerfile
-
-	docker run --rm -e PYTHONPATH=/ -e PYTHONIOENCODING=UTF-8 -w / --user=`id -u`:`id -g` -v ${APP_PATH}/pathology:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data ${PATHOLOGY_DOCKER_IMAGE_NAME} /usr/bin/python3 /app/dataAnalyzer.py /config/conf.json 
-	exit 0
-
-elif [ $# -eq 2 -a $1 == 'interactive' ]
-then
-	if [ $2 == 'root' ]
-	then
-		docker run -it --rm --user=0:0 -w /home/myuser -v "$(pwd)":/app  -v "${DATA_PATH}":/data:rw --entrypoint /bin/bash  ${COOCCUR_DOCKER_IMAGE_NAME}
-	else
-		docker run -it --rm --user=1968:1968 -w /home/myuser -v "$(pwd)":/app  -v "${DATA_PATH}":/data:rw --entrypoint /bin/bash  ${COOCCUR_DOCKER_IMAGE_NAME}
-	fi
-
-elif [ $# -eq 11 ]
-then
-
-	VCF_FILE=$1
-	OUTPUT_FILE=$2
-	HG_VERSION=$3
-	ENSEMBL_RELEASE=$4
-	CHROM=$5
-	PHASED=$6
-	GENE=$7
-	SAVE_VARS=$8
-        BRCA_VARS=$9
-	IPV_FILE=${10}
-	VPI_FILE=${11}
-
-
-	docker build -t ${COOCCUR_DOCKER_IMAGE_NAME} - < docker/cooccurrence/Dockerfile
-
-	docker run --rm -e PYTHONPATH=/ -e PYTHONIOENCODING=UTF-8 --user=`id -u`:`id -g` -v ${APP_PATH}/cooccurrence:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data:rw ${COOCCUR_DOCKER_IMAGE_NAME} /usr/bin/python3 /app/cooccurrenceFinder.py  --v ${VCF_FILE} --o ${OUTPUT_FILE} --h $HG_VERSION --e $ENSEMBL_RELEASE --c $CHROM --p $PHASED --s ${SAVE_VARS} --g $GENE --b $BRCA_VARS  --d /var/tmp/pyensembl-cache --ipv $IPV_FILE  --vpi $VPI_FILE
-
-
-else
-	echo "wrong usage"
-	exit 1
-fi
+TOP_DIR=/private/groups/patenlab/jcasalet/federated-analysis
+PYTHON_SCRIPT=${TOP_DIR}/app/cooccurrence/cooccurrenceFinder.py
+miniwdl cromwell /private/groups/patenlab/jcasalet/WDL/myVusCooccur.wdl \
+PYTHON_SCRIPT=${PYTHON_SCRIPT} \
+VCF_FILE=${DATA_DIR}/$VCF_FILENAME \
+ANNO_FILE=${DATA_DIR}/freeze8_sample_annot_2020-07-07.txt \
+BRCA_FILE=${TOP_DIR}/data/brca-variants.tsv \
+OUTPUT_FILENAME=${CHROM}-out.json \
+ALL_FILENAME=${CHROM}-all.json \
+VPI_FILENAME=${CHROM}-vpi.json \
+IPV_FILENAME=${CHROM}-ipv.json \
+TOUT_FILENAME=${CHROM}-tout.json \
+HG_VERSION=38 \
+FREEZE=8 \
+ENSEMBL_RELEASE=99 \
+PHASED=True \
+CHROM=$CHROM \
+GENE=$GENE \
+NUM_CORES=$(grep -c processor /proc/cpuinfo) \
+-c /private/groups/patenlab/jcasalet/WDL/cromwell.local.conf 
