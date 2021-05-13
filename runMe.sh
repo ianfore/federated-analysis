4232d705d5da446e5d67b27b53696d15a09bd993
James Casaletto <jcasaletto@maprtech.com>
2021-05-13 10:49:49 -0700

    refactor bash run script to use args

--- runMe.sh
+++ runMe.sh
@@ -9,51 +9,96 @@ PATHOLOGY_DOCKER_IMAGE_NAME=brcachallenge/federated-analysis:pathology
 
 if [ $# -eq 0 ]
 then
-	echo "usage: $0 <vcf-file> analyze"
-	echo "OR"
-	echo "$0 <input-vcf-filename> <hg-version> <ensembl-version> <chromosome-of-interest> <phased-boolean> <gene-of-interest> <brca-vars-filename> <save-files-boolean>" 
-	echo "example: $0 bc3.vcf 38 99 13 True BRCA2 brca-variants.tsv True" 
+	echo "usage: $0 -dq qualityReportConfigFile -vf vcfFile -hg hgVersion -er ensemblRelease -c chromosome -p phased -g gene -vp variantPathogenicityFile -dd dataDirectory -st saveTempfiles -sp samplePathologyFile"
+	echo "example: $0 -dq quality-report-config.json -vf my.vcf -hg 38 -er 99 -c 13 -p True -g BRCA2 -vp brca-variants.tsv -dd data -st True -sp mypf.tsv " 
 	exit 1
 	
 
-elif [ $# -eq 1 -a $1 == 'analyze' ]
-then
+else
+	for arg in "$@"
+	do
+    		case $arg in
+			-dq |--dataQuality)
+			CONFIG_FILE="$2"
+        		shift # Remove argument name from processing
+        		shift # Remove argument value from processing
+			docker build -t ${PATHOLOGY_DOCKER_IMAGE_NAME} - < docker/pathology/Dockerfile
+			docker run --rm -e PYTHONPATH=/ -e PYTHONIOENCODING=UTF-8 -w / --user=`id -u`:`id -g` -v ${APP_PATH}/pathology:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data ${PATHOLOGY_DOCKER_IMAGE_NAME} /usr/bin/python3 /app/dataAnalyzer.py -c /config/$CONFIG_FILE 
+			;;
+			
 
-	docker build -t ${PATHOLOGY_DOCKER_IMAGE_NAME} - < docker/pathology/Dockerfile
+			-vf|--vcfFile)
+			VCF_FILE="$2"
+        		shift # Remove argument name from processing
+        		shift # Remove argument value from processing
+        		;;
 
-	docker run --rm -e PYTHONPATH=/ -e PYTHONIOENCODING=UTF-8 -w / --user=`id -u`:`id -g` -v ${APP_PATH}/pathology:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data ${PATHOLOGY_DOCKER_IMAGE_NAME} /usr/bin/python3 /app/dataAnalyzer.py /config/conf.json 
-	exit 0
+			-hg|--hgVersion)
+			HG_VERSION="$2"
+        		shift # Remove argument name from processing
+        		shift # Remove argument value from processing
+        		;;
 
-elif [ $# -eq 2 -a $1 == 'interactive' ]
-then
-	if [ $2 == 'root' ]
-	then
-		docker run -it --rm --user=0:0 -w /home/myuser -v "$(pwd)":/app  -v "${DATA_PATH}":/data:rw --entrypoint /bin/bash  ${COOCCUR_DOCKER_IMAGE_NAME}
-	else
-		docker run -it --rm --user=1968:1968 -w /home/myuser -v "$(pwd)":/app  -v "${DATA_PATH}":/data:rw --entrypoint /bin/bash  ${COOCCUR_DOCKER_IMAGE_NAME}
-	fi
+			-er|--ensemblRelease)
+			ENSEMBL_RELEASE="$2"
+        		shift # Remove argument name from processing
+        		shift # Remove argument value from processing
+        		;;
 
-elif [ $# -eq 10 ]
-then
-	
-	VCF_FILE=$1
-	HG_VERSION=$2
-	ENSEMBL_RELEASE=$3
-	CHROM=$4
-	PHASED=$5
-	GENE=$6
-        BRCA_VARS=$7
-	DATA_DIR=$8
-	SAVE_FILES=$9
-	PATHOLOGY_FILE=${10}
+			-c|--chromosome)
+			CHROM="$2"
+        		shift # Remove argument name from processing
+        		shift # Remove argument value from processing
+        		;;
+
+        		-p|--phased)
+        		PHASED="$2"
+        		shift # Remove argument name from processing
+        		shift # Remove argument value from processing
+        		;;
+
+			-g|--gene)
+			GENE="$2"
+        		shift # Remove argument name from processing
+        		shift # Remove argument value from processing
+        		;;
+
+			-pf|--pathogenicityFile)
+			PATHOGENICITY_FILE="$2"
+        		shift # Remove argument name from processing
+        		shift # Remove argument value from processing
+        		;;
+
+
+			-dd|--dataDirectory)
+			DATA_DIR="$2"
+        		shift # Remove argument name from processing
+        		shift # Remove argument value from processing
+        		;;
+
+			-st|--saveTempfiles)
+			SAVE_FILES="$2"
+        		shift # Remove argument name from processing
+        		shift # Remove argument value from processing
+        		;;
+
+			-sp|--samplePathologyFile)
+			PATHOLOGY_FILE="$2"
+        		shift # Remove argument name from processing
+        		shift # Remove argument value from processing
+        		;;
+
+        		*)
+			#echo "wrong usage: $0 $@"
+			#exit 1
+        		;;
+    		esac
+	done	
 
 
 	docker build -t ${COOCCUR_DOCKER_IMAGE_NAME} - < docker/cooccurrence/Dockerfile
 
-	docker run --rm -e PYTHONPATH=/ -e PYTHONIOENCODING=UTF-8 --user=`id -u`:`id -g` -v ${APP_PATH}/cooccurrence:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data:rw ${COOCCUR_DOCKER_IMAGE_NAME} /usr/bin/python3 /app/cooccurrenceFinder.py  --vcf $VCF_FILE --h $HG_VERSION --e $ENSEMBL_RELEASE --c $CHROM --p $PHASED  --g $GENE --b $BRCA_VARS  --d /var/tmp/pyensembl-cache  --data /data --save $SAVE_FILES --pf $PATHOLOGY_FILE
+	docker run --rm -e PYTHONPATH=/ -e PYTHONIOENCODING=UTF-8 --user=`id -u`:`id -g` -v ${APP_PATH}/cooccurrence:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data:rw ${COOCCUR_DOCKER_IMAGE_NAME} /usr/bin/python3 /app/cooccurrenceFinder.py  --vcf $VCF_FILE --h $HG_VERSION --e $ENSEMBL_RELEASE --c $CHROM --p $PHASED  --g $GENE --b $PATHOGENICITY_FILE  --d /var/tmp/pyensembl-cache  --data /data --save $SAVE_FILES --pf $PATHOLOGY_FILE
 
 
-else
-	echo "wrong usage"
-	exit 1
 fi
